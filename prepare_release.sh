ee8712d9647e0e613879564ec3e878b2a2bf02a8
Ilan COSTA <ilan.costa@freepro.com>
2025-09-29 00:24:45 +0200

    Personnalisation du fichier de release

--- prepare_release.sh
+++ prepare_release.sh
@@ -12,16 +12,4 @@ do_prepare() {
   cp openwrt/bin/targets/$3/$4/chirpstack-gateway-os-$1-$6-$3-$4-$5* dist/$1/$2/$3/$4
 }
 
-do_prepare $1 raspberrypi bcm27xx bcm2708 rpi base
-do_prepare $1 raspberrypi bcm27xx bcm2708 rpi full
-do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 base
-do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 full
-do_prepare $1 raspberrypi bcm27xx bcm2712 rpi-5 base
-do_prepare $1 raspberrypi bcm27xx bcm2712 rpi-5 full
-do_prepare $1 rak bcm27xx bcm2709 rpi-2 rak7391
-do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7268v2
-do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7289v2
-do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7267
-do_prepare $1 seeed ramips mt76x8 sensecap_wm7628n sensecap-m2
-do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 lacyber-base
-do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 lacyber-full
+do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 lacyber
